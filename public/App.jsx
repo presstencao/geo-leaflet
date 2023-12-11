4f63b6fd2f541aa726f1d7b02c93a9d9ff4377e1
presstencao <presstencao@gmail.com>
2023-12-11 01:02:38 -0300

    Fix Bugs

--- public/App.jsx
+++ public/App.jsx
@@ -1,6 +1,6 @@
 //App.js
 import React, { useEffect, useState } from 'react';
-import { ErrorComponent, GetLatLngGoogleUrl, PointMap } from '../src';
+import{GetLatLngGoogleUrl, ErrorComponent, PointMap} from 'geo-leaflet'
 
 
 
