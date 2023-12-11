f21bde208196d4c3c5850ce147014d448ed1a1d6
presstencao <presstencao@gmail.com>
2023-12-10 21:30:25 -0300

    initial Commit

---
+++ public/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import GetLatLngGoogleUrl from '../src/components/GetLatLngGoogleUrl'
+
+ const App = () => {
+  return (
+    <GetLatLngGoogleUrl />
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/components/GetLatLngGoogleUrl.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const GetLatLngGoogleUrl = props => {
+  return (
+    <div>GetLatLngGoogleUrl</div>
+  )
+}
+
+GetLatLngGoogleUrl.propTypes = {}
+
+export default GetLatLngGoogleUrl
